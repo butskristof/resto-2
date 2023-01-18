498df7e31be06ec3da537dc5aef162c35e4dfed6
Kristof Buts <kristof.buts@marqiz.com>
2023-01-18 23:33:10 +0100

    working order page

--- resto-client/src/components/order/current/CurrentOrder.vue
+++ resto-client/src/components/order/current/CurrentOrder.vue
@@ -1,11 +1,25 @@
 <template>
-  <pre>{{ JSON.stringify(currentOrder, null, 2) }}</pre>
+  <CurrentOrderTicket />
+  <div>Total: {{ currentOrderTotal }}</div>
+  <label>
+    Cash received:
+    <input type="number" v-model.number="cashReceived" />
+  </label>
+  <div>To return: {{ cashToReturn }}</div>
+  <div><button type="button">order</button></div>
 </template>
 
 <script setup>
+import CurrentOrderTicket from '@/components/order/current/CurrentOrderTicket.vue';
+import { storeToRefs } from 'pinia';
 import { useCurrentOrderStore } from '@/stores/current-order';
+import { computed, ref } from 'vue';
 
-const { currentOrder } = useCurrentOrderStore();
+const { currentOrderTotal } = storeToRefs(useCurrentOrderStore());
+const cashReceived = ref(0);
+const cashToReturn = computed(
+  () => cashReceived.value - currentOrderTotal.value,
+);
 </script>
 
 <style scoped></style>
--- resto-client/src/stores/current-order.js
+++ resto-client/src/stores/current-order.js
@@ -1,8 +1,45 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { useProductsStore } from '@/stores/products';
 
 export const useCurrentOrderStore = defineStore('current-order', () => {
   const currentOrder = ref([]);
+
+  const extendedCurrentOrder = computed(() => {
+    const { products } = useProductsStore();
+    return currentOrder.value.map((orderLine) => {
+      const product = products.find((p) => p.id === orderLine.productId);
+      const toppings = product.toppings.filter((t) =>
+        orderLine.toppingIds.includes(t.id),
+      );
+
+      let description = product.name;
+      if (toppings.length > 0) {
+        description += `, ${toppings.map((t) => t.name).join(', ')}`;
+      }
+
+      const unitPrice =
+        product.price +
+        toppings.reduce(
+          (runningTotal, topping) => runningTotal + topping.price,
+          0,
+        );
+
+      const totalPrice = orderLine.count * unitPrice;
+      return {
+        description,
+        count: orderLine.count,
+        unitPrice,
+        totalPrice,
+      };
+    });
+  });
+  const currentOrderTotal = computed(() => {
+    return extendedCurrentOrder.value.reduce(
+      (runningTotal, orderLine) => runningTotal + orderLine.totalPrice,
+      0,
+    );
+  });
 
   function addToOrder({ productId, toppingIds }) {
     const existing = currentOrder.value.find(
@@ -22,5 +59,22 @@ export const useCurrentOrderStore = defineStore('current-order', () => {
     }
   }
 
-  return { currentOrder, addToOrder };
+  function increment(index) {
+    currentOrder.value[index].count += 1;
+  }
+  function decrement(index) {
+    const item = currentOrder.value[index];
+    item.count -= 1;
+    if (item.count === 0) currentOrder.value.splice(index, 1);
+  }
+
+  return {
+    currentOrder,
+    extendedCurrentOrder,
+    currentOrderTotal,
+
+    addToOrder,
+    increment,
+    decrement,
+  };
 });
