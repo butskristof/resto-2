10f3a1dbf95379380e16f32038d7341bc244c45e
Kristof <git@kristofbuts.be>
2023-02-17 21:33:46 +0100

    show stats with chart

--- client/src/pages/StatsPage.vue
+++ client/src/pages/StatsPage.vue
@@ -1,31 +1,16 @@
 <template>
   <div class="stats-page">
-    <LoadingIndicator v-if="loading">Statistieken laden</LoadingIndicator>
-    <div v-if="isError" class="error">{{ error }}</div>
-    <div v-if="isSuccess">
-      <ul>
-        <li v-for="product in data.products" :key="product.id">
-          {{ product.product.name }}: {{ product.quantity }}
-          <ul v-if="product.toppings.length > 0">
-            <li v-for="topping in product.toppings" :key="topping.topping.id">
-              {{ topping.topping.name }}: {{ topping.quantity }}
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-    <!--    <pre v-if="isSuccess">{{ JSON.stringify(data, null, 2) }}</pre>-->
+    <OrderStatistics />
   </div>
 </template>
 
-<script setup>
-import { useOrderStatisticsQuery } from '@/composables/queries';
-import LoadingIndicator from '@/components/common/LoadingIndicator.vue';
-import { computed } from 'vue';
+<script>
+import { defineComponent } from 'vue';
+import OrderStatistics from '@/components/order-stats/OrderStatistics.vue';
 
-const { data, isLoading, isFetching, isSuccess, isError, error } =
-  useOrderStatisticsQuery();
-const loading = computed(() => isLoading.value || isFetching.value);
+export default defineComponent({
+  components: { OrderStatistics },
+});
 </script>
 
 <style scoped lang="scss">
