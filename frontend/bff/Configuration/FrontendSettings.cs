d9a283d904402ac3ff277e8ca88e1db2b471955f
Kristof Buts <git@kristofbuts.be>
2025-02-09 12:52:09 +0100

    don't include bff

--- frontend/bff/Configuration/FrontendSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Resto.Bff.Configuration;
-
-internal sealed class FrontendSettings
-{
-    internal const string SectionName = "Frontend";
-
-    public string? ClientUrl { get; init; }
-    public Uri? ClientUri => Uri.TryCreate(ClientUrl, UriKind.Absolute, out var uri) ? uri : null;
-}
