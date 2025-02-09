99d3aab3a1cc6c6f55ab73e855f17fde0a374931
Kristof Buts <git@kristofbuts.be>
2025-02-09 13:05:20 +0100

    add devtools to vite

--- frontend/spa/vite.config.js
+++ frontend/spa/vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [vue(), vueDevTools(), eslintPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
