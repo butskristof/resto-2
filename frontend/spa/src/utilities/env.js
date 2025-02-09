b082ed91f28c86a9dc52b81f15634f1af108116e
Kristof Buts <git@kristofbuts.be>
2025-02-09 18:07:26 +0100

    remove old Docker setup from SPA

--- frontend/spa/src/utilities/env.js
+++
@@ -1,7 +0,0 @@
-// prefer the value from window.configs (injected in the entrypoint script)
-// or fallback to the usual env vars during build
-function getEnv(name) {
-  return window?.configs?.[name] || import.meta.env[name];
-}
-
-export const RESTO_API_BASE_URL = getEnv('VITE_RESTO_API_BASE_URL');
